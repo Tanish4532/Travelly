356f9531932da7c70e1ab5cbe73401e18f621c8e
TANISH PANDEY <60178885+Tanish4532@users.noreply.github.com>
2021-08-09 00:04:46 +0530

    updated index.php file
    In this file,I have created another button which for checking out the forms and by clicking on the button ,we will see the details of those who have filled the forms.

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
       href="https://fonts.googleapis.com/css2?family=Anton&family=MonteCarlo&display=swap"
       rel="stylesheet"
     />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   </head>
   <body>
     <img class="bg" src="bg.jpg" alt="travelly" />
@@ -52,7 +55,9 @@
           rows="10"
           placeholder="Enter any other information"
         ></textarea>
+        <br>
         <button type="submit" class="btn btn-primary" name="submit">Register</button>
+        <a class="btn btn-success" href="display.php">Check Form</a>
       </form>
     </div>
     
